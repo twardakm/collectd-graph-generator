629d79d56f460908caa71410eaf0f063076fd8f1
Marcin Twardak <twardakm@gmail.com>
2020-11-07 22:03:30 +0100

    Add support to spaces in processes names

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use cgg::config::Config;
 use clap::{load_yaml, App};
 
+const EXAMPLES: &'static str = &"EXAMPLES:
+    ./cgg -i /var/lib/collectd/marcin-manjaro/ -t \"last 4 hours\"\n
+    ./cgg --input marcin@localhost:/var/lib/collectd/marcin-manjaro/ \\
+-t \"last 10 days\" -w 2048 -h 1024 -o processes.png\n
+    ./cgg -i marcin@192.168.0.163:/var/lib/collectd/marcin-manjaro/ \\
+-t \"last 1 hour\" --processes \"firefox,spotify,visual studio code\"";
+
 fn main() {
     let yaml = load_yaml!("cli.yml");
-    let cli = App::from(yaml).get_matches();
+    let cli = App::from(yaml).after_help(EXAMPLES).get_matches();
 
     let config = match Config::new(&cli) {
         Ok(config) => config,
